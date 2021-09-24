936398875a8773a6ec3360015e673533592fb495
Yauheni <ev.drozd.2020@gmail.com>
2021-09-24 23:57:03 +0300

    eighth homework

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,40 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => {
     switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
+        case 'SORT-UP': {
+            let copy = [...state]
+           return copy.sort((a, b) => a.age < b.age ? 1 : -1)
         }
-        case 'check': {
-            // need to fix
-            return state
+        case 'SORT-DOWN': {
+            let copy = [...state]
+
+            return copy.sort((a, b) => a.age > b.age ? 1 : -1)
+        }
+        case 'CHECK': {
+            let copy = [...state]
+
+            return copy.filter(p => p.age > 18)
         }
         default: return state
+    }
+}
+
+type ActionType = ReturnType<typeof sortUpAC> | ReturnType<typeof sortDownAC> | ReturnType<typeof checkAC>
+
+
+export const sortUpAC = () => {
+    return {
+        type: 'SORT-UP',
+    }
+}
+export const sortDownAC = () => {
+    return {
+        type: 'SORT-DOWN',
+    }
+}
+export const checkAC = () => {
+    return {
+        type: 'CHECK',
     }
 }
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import {homeWorkReducer} from '../homeWorkReducer'
+import {checkAC, homeWorkReducer, sortDownAC, sortUpAC} from '../homeWorkReducer'
+import {UserType} from '../../HW8';
 
-let initialState: any[] // need to fix any
+let initialState: Array<UserType> // need to fix any
 
 beforeEach(() => {
     initialState = [
@@ -15,18 +16,35 @@ beforeEach(() => {
 })
 
 test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+    const newState = homeWorkReducer(initialState, sortUpAC())
 
     console.log(newState)
-    // expect(...).toBe(...)
+    expect(newState[0].age).toBe(66)
+    expect(newState[1].age).toBe(55)
+    expect(newState[2].age).toBe(44)
+    expect(newState[3].age).toBe(40)
+    expect(newState[4].age).toBe(16)
+    expect(newState[5].age).toBe(3)
+
 })
 test('sort name down', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+    const newState = homeWorkReducer(initialState, sortDownAC())
 
-
+    expect(newState[0].age).toBe(3)
+    expect(newState[1].age).toBe(16)
+    expect(newState[2].age).toBe(40)
+    expect(newState[3].age).toBe(44)
+    expect(newState[4].age).toBe(55)
+    expect(newState[5].age).toBe(66)
 })
 test('check age 18', () => {
-    const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
+    const newState = homeWorkReducer(initialState, checkAC())
 
-
+    expect(newState.length).toBe(4)
+    expect(newState).toEqual([
+        {_id: 1, name: 'Александр', age: 66},
+        {_id: 3, name: 'Виктор', age: 44},
+        {_id: 4, name: 'Дмитрий', age: 40},
+        {_id: 5, name: 'Ирина', age: 55},
+    ])
 })
