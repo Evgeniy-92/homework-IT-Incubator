2db10bccd53e87cc5c63f47da93896a37bcde079
Yauheni <ev.drozd.2020@gmail.com>
2021-09-24 21:21:27 +0300

    sixth homework

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -10,7 +10,8 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        const newValue = restoreState<string>('editable-span-value', '')
+        setValue(newValue)
     }
 
     return (
